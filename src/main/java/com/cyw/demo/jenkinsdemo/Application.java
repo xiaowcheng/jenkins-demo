815c22eb7652bd006ec965e3da6abbe8a2527bfc
xiaowcheng <984059736@qq.com>
2019-04-18 14:49:37 +0800

    Initial commit

---
+++ src/main/java/com/cyw/demo/jenkinsdemo/Application.java
@@ -0,0 +1,20 @@
+package com.cyw.demo.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("jenkins")
+    public String jenkins(){
+        return "hello jenkins";
+    }
+}
